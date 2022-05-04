9ed27f8d0ab41e7d333856057f783018140bdcd7
Yuri Pieters <magejohnyjtp@gmail.com>
2022-05-04 22:57:33 +0100

    Add scripts and notes for obtaining data & code
    Size and licensing worries make it easier to do this rather than including the files directly.

---
+++ Datasets/download_datasets.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Note that this has only been lightly tested. Hopefully if it breaks it still
+# servers as documentationo the required links.
+
+# IMPORTANT: this will expire after Sun, 15 May 2022 10:58:08 GMT. If this is
+# needed after it does, download one of the links below in a browser, fill in
+# the form, and copy the new sessionid cookie here.
+cookies='Cookie: sessionid=62b0334bd44139fe0a920747af44cb49'
+
+links=(
+  "https://ibug.doc.ic.ac.uk/download/annotations/300w.zip.001"
+  "https://ibug.doc.ic.ac.uk/download/annotations/300w.zip.002"
+  "https://ibug.doc.ic.ac.uk/download/annotations/300w.zip.003"
+  "https://ibug.doc.ic.ac.uk/download/annotations/300w.zip.004"
+  "https://ibug.doc.ic.ac.uk/download/annotations/300w_cropped.zip"
+  "https://ibug.doc.ic.ac.uk/download/annotations/afw.zip"
+  "https://ibug.doc.ic.ac.uk/download/annotations/helen.zip"
+  "https://ibug.doc.ic.ac.uk/download/annotations/ibug.zip"
+  "https://ibug.doc.ic.ac.uk/download/annotations/lfpw.zip"
+)
+
+sets=("300w" "300w_cropped" "afw" "helen" "ibug" "lfpw")
+
+for link in "${links[@]}"
+do
+  echo "Downloading ${link}"
+  curl -L -O "${link}" -H "${cookies}"
+done
+
+for set in "${sets[@]}"
+do
+  echo unzip $set -d $set
+done
